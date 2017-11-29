c67d9d5a5100c61bad3eba4826dc7906b2b3495d
ipheghe <iphegheovie@gmail.com>
2017-11-29 18:16:19 +0100

    mend-no-edit

--- client/src/store/configure-store.js
+++ client/src/store/configure-store.js
@@ -1,24 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
-
-const logger = createLogger({
-  // options
-});
 
 /**
  * @export configureStore
  * @func
  * @returns {*} void
  */
-const configureStore = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return composeWithDevTools(applyMiddleware(thunk))(createStore)(rootReducer);
-  }
-  return composeWithDevTools(applyMiddleware(thunk, logger))(createStore)(rootReducer);
-};
+const configureStore = () => composeWithDevTools(applyMiddleware(thunk))(createStore)(rootReducer);
 
 
 export default configureStore;
