23f8b98ba2900da257af00ce28e2511fde1ab77b
ipheghe <iphegheovie@gmail.com>
2018-01-22 16:56:28 +0100

    mend-no-edit

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ app.use(bodyParser.json({ type: 'application/json' }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, ' +
+    'Authorization, Access-Control-Allow-Credentials'
+  );
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
